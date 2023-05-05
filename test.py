d0404c8ea25b831389f45805d16d508767e72ef0
Mario Geiger <geiger.mario@gmail.com>
2023-05-05 16:39:47 -0400

    test

--- test.py
+++ test.py
@@ -5,65 +5,16 @@ import jax
 import jax.numpy as jnp
 import jraph
 
-from nequip_jax import NEQUIPLayerFlax, NEQUIPLayerHaiku
+from nequip_jax import (
+    NEQUIPLayerFlax,
+    NEQUIPLayerHaiku,
+    NEQUIPESCNLayerFlax,
+    NEQUIPESCNLayerHaiku,
+)
 
 
-class NEQUIP(flax.linen.Module):
-    @flax.linen.compact
-    def __call__(self, graph: jraph.GraphsTuple) -> jraph.GraphsTuple:
-        positions = graph.nodes["positions"]
-        species = graph.nodes["species"]
-
-        vectors = e3nn.IrrepsArray(
-            "1o", positions[graph.receivers] - positions[graph.senders]
-        )
-        node_feats = flax.linen.Embed(num_embeddings=5, features=32)(species)
-        node_feats = e3nn.IrrepsArray(f"{node_feats.shape[1]}x0e", node_feats)
-
-        for _ in range(3):
-            layer = NEQUIPLayerFlax(
-                avg_num_neighbors=1.0,
-            )
-            node_feats = layer(
-                vectors,
-                node_feats,
-                species,
-                graph.senders,
-                graph.receivers,
-            )
-
-        return node_feats
-
-
-@hk.without_apply_rng
-@hk.transform
-def hk_model(graph: jraph.GraphsTuple) -> jraph.GraphsTuple:
-    positions = graph.nodes["positions"]
-    species = graph.nodes["species"]
-
-    vectors = e3nn.IrrepsArray(
-        "1o", positions[graph.receivers] - positions[graph.senders]
-    )
-    node_feats = hk.Embed(vocab_size=5, embed_dim=32)(species)
-    node_feats = e3nn.IrrepsArray(f"{node_feats.shape[1]}x0e", node_feats)
-
-    for _ in range(3):
-        layer = NEQUIPLayerHaiku(
-            avg_num_neighbors=1.0,
-        )
-        node_feats = layer(
-            vectors,
-            node_feats,
-            species,
-            graph.senders,
-            graph.receivers,
-        )
-
-    return node_feats
-
-
-def main(model):
-    graph = jraph.GraphsTuple(
+def dummy_graph():
+    return jraph.GraphsTuple(
         nodes={
             "positions": jnp.array([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0]]),
             "species": jnp.array([0, 1]),
@@ -76,22 +27,151 @@ def main(model):
         n_edge=jnp.array([2]),
     )
 
+
+def test_nequip_flax():
+    class NEQUIP(flax.linen.Module):
+        @flax.linen.compact
+        def __call__(self, graph: jraph.GraphsTuple) -> jraph.GraphsTuple:
+            positions = graph.nodes["positions"]
+            species = graph.nodes["species"]
+
+            vectors = e3nn.IrrepsArray(
+                "1o", positions[graph.receivers] - positions[graph.senders]
+            )
+            node_feats = flax.linen.Embed(num_embeddings=5, features=32)(species)
+            node_feats = e3nn.IrrepsArray(f"{node_feats.shape[1]}x0e", node_feats)
+
+            for _ in range(2):
+                layer = NEQUIPLayerFlax(
+                    avg_num_neighbors=1.0, output_irreps="16x0e + 16x1o"
+                )
+                node_feats = layer(
+                    vectors,
+                    node_feats,
+                    species,
+                    graph.senders,
+                    graph.receivers,
+                )
+
+            return node_feats
+
+    graph = dummy_graph()
+
+    model = NEQUIP()
     w = model.init(jax.random.PRNGKey(0), graph)
 
-    # print(jax.tree_util.tree_map(lambda x: x.shape, w))
-    print("number of parameters:", sum(x.size for x in jax.tree_util.tree_leaves(w)))
+    apply = jax.jit(model.apply)
+    apply(w, graph)
+    apply(w, graph)
+
+
+def test_nequip_haiku():
+    @hk.without_apply_rng
+    @hk.transform
+    def model(graph: jraph.GraphsTuple) -> jraph.GraphsTuple:
+        positions = graph.nodes["positions"]
+        species = graph.nodes["species"]
+
+        vectors = e3nn.IrrepsArray(
+            "1o", positions[graph.receivers] - positions[graph.senders]
+        )
+        node_feats = hk.Embed(vocab_size=5, embed_dim=32)(species)
+        node_feats = e3nn.IrrepsArray(f"{node_feats.shape[1]}x0e", node_feats)
+
+        for _ in range(3):
+            layer = NEQUIPLayerHaiku(
+                avg_num_neighbors=1.0,
+                output_irreps="16x0e + 16x1o",
+            )
+            node_feats = layer(
+                vectors,
+                node_feats,
+                species,
+                graph.senders,
+                graph.receivers,
+            )
+
+        return node_feats
+
+    graph = dummy_graph()
+
+    w = model.init(jax.random.PRNGKey(0), graph)
 
     apply = jax.jit(model.apply)
-
-    print("compiling")
+    apply(w, graph)
     apply(w, graph)
 
-    print("running")
+
+def test_nequip_escn_flax():
+    class NEQUIP(flax.linen.Module):
+        @flax.linen.compact
+        def __call__(self, graph: jraph.GraphsTuple) -> jraph.GraphsTuple:
+            positions = graph.nodes["positions"]
+            species = graph.nodes["species"]
+
+            vectors = e3nn.IrrepsArray(
+                "1o", positions[graph.receivers] - positions[graph.senders]
+            )
+            node_feats = flax.linen.Embed(num_embeddings=5, features=32)(species)
+            node_feats = e3nn.IrrepsArray(f"{node_feats.shape[1]}x0e", node_feats)
+
+            for _ in range(2):
+                layer = NEQUIPESCNLayerFlax(
+                    avg_num_neighbors=1.0,
+                    output_irreps="16x0e + 16x1o",
+                )
+                node_feats = layer(
+                    vectors,
+                    node_feats,
+                    species,
+                    graph.senders,
+                    graph.receivers,
+                )
+
+            return node_feats
+
+    graph = dummy_graph()
+
+    model = NEQUIP()
+    w = model.init(jax.random.PRNGKey(0), graph)
+
+    apply = jax.jit(model.apply)
+    apply(w, graph)
     apply(w, graph)
 
-    print("done")
 
+def test_nequip_escn_haiku():
+    @hk.without_apply_rng
+    @hk.transform
+    def model(graph: jraph.GraphsTuple) -> jraph.GraphsTuple:
+        positions = graph.nodes["positions"]
+        species = graph.nodes["species"]
 
-if __name__ == "__main__":
-    main(NEQUIP())
-    main(hk_model)
+        vectors = e3nn.IrrepsArray(
+            "1o", positions[graph.receivers] - positions[graph.senders]
+        )
+        node_feats = hk.Embed(vocab_size=5, embed_dim=32)(species)
+        node_feats = e3nn.IrrepsArray(f"{node_feats.shape[1]}x0e", node_feats)
+
+        for _ in range(3):
+            layer = NEQUIPESCNLayerHaiku(
+                avg_num_neighbors=1.0,
+                output_irreps="16x0e + 16x1o",
+            )
+            node_feats = layer(
+                vectors,
+                node_feats,
+                species,
+                graph.senders,
+                graph.receivers,
+            )
+
+        return node_feats
+
+    graph = dummy_graph()
+
+    w = model.init(jax.random.PRNGKey(0), graph)
+
+    apply = jax.jit(model.apply)
+    apply(w, graph)
+    apply(w, graph)
