8394fda8643c6703956f232989838c760167bd1a
Mario Geiger <geiger.mario@gmail.com>
2023-06-22 18:36:07 -0400

    Update __init__.py

--- nequip_jax/__init__.py
+++ nequip_jax/__init__.py
@@ -1,7 +1,7 @@
 from .nequip import NEQUIPLayerFlax, NEQUIPLayerHaiku
 from .nequip_escn import NEQUIPESCNLayerFlax, NEQUIPESCNLayerHaiku
 
-__version__ = "1.0.2"
+__version__ = "1.1.0"
 
 __all__ = [
     "NEQUIPLayerFlax",
